3f185a4c56209773c21e589925a3157570a02eb1
Priyanka <127305536+NERSUPALLIPRIYANKA@users.noreply.github.com>
2023-05-03 18:55:22 +0530

    Add files via upload

---
+++ javascriptt.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head><TITLE>life is too short to argue...say get lost to everyon</TITLE></head>
+<body>
+<script type="text/javascript">
+document.write("MY LIFE");
+</script>
+</body>
+</html>
